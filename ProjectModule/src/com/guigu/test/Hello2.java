f2f40f89b9d8b48fb8d6f9823aeea92768170e19
lx275956146 <275956146@qq.com>
2019-02-15 17:02:22 +0800

    Initial commit

---
+++ ProjectModule/src/com/guigu/test/Hello2.java
@@ -0,0 +1,31 @@
+package com.guigu.test;
+
+/**
+ * @author lixiang
+ * @create 2019-02-15 15:52
+ */
+public class Hello2 {
+    public static final int sbc = 10;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void testUpdate(){
+
+    }
+    /**
+     * 客户的id
+     */
+     private int id = 1;
+     
+     /**
+      * 客户的姓名
+      */
+      private String name;
+
+
+
+      
+
+}
---
+++ src/com/guigu/test/Hello.java
@@ -0,0 +1,7 @@
+package com.guigu.test;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("还有几天就要开学了");
+    }
+}
